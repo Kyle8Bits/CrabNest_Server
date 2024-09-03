5f20b6408b3e76244538f145c06c285f41ba1d6b
s3979194 <s3979194@rmit.edu.vn>
2024-09-03 23:20:37 +0700

    comm

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,7 +4,9 @@ const Post = require('../models/Post');
 // Fetch all comments for a specific post
 exports.getCommentsForPost = async (req, res) => {
     try {
-        const comments = await Comment.find({ post: req.params.postId }).populate('author', 'name avatar');
+        const { postId } = req.query;
+        console.log(postId)
+        const comments = await Comment.find({ post: postId }).populate('author', 'name avatar');
         res.status(200).json(comments);
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -14,6 +16,7 @@ exports.getCommentsForPost = async (req, res) => {
 // Add a comment to a specific post
 exports.addCommentToPost = async (req, res) => {
     try {
+        console.log(req.params)
         const comment = new Comment({
             post: req.params.postId,
             author: req.body.author, // Assuming author ID is passed in the request body
